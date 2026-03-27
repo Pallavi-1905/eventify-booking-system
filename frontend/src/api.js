136e2d6d2a0c7e8f6459a148ce665eff85d8dea9
Pallavi-1905 <vadlapallavi@gmail.com>
2026-03-27 20:19:54 +0530

    Prepare frontend for Render deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://eventify-booking-backend.onrender.com",
   headers: { "Content-Type": "application/json" },
   timeout: 10000,
 });
